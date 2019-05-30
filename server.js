81f9e67d670a2307862519183bb2af605bfcb57e
umeshredd <47346567+umeshredd@users.noreply.github.com>
2019-05-30 19:38:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -65,11 +65,17 @@ app.use((req, res, next) => {
 app.use("/api/posts", posts);
 
 //@router test
-app.get("/", (req, res) =>
-  res.json({
+app.get("/", (req, res) =>{
+  if(process.env.an_env_var == ""){
+     res.send(404)
+  }else{
+    res.json({
     greet: "hello"
   })
-);
+  }
+  
+
+});
 
 app.listen(PORT, () => {
   console.log("Server is running at port", +PORT);
