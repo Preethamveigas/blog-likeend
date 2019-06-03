4db1db662ab3df9708c53153e8034fbee9f43289
umeshredd <47346567+umeshredd@users.noreply.github.com>
2019-06-03 17:16:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ app.use((req, res, next) => {
 app.use("/api/posts", posts);
 
 //@router test
-app.get("/", (req, res) =>{
+app.get("/backend-like", (req, res) =>{
   var s = process.env.an_env_var || ""; 
   if(s == ""){
      console.log("hello",s)
